648d4b179e51e6369e88044249521e067d4f0b76
Ricardo Montesinos <ricardo.montesinos@globant.com>
2018-09-25 15:38:11 -0500

    Projec creation. Build succeded

---
+++ AutoCommitter/AutoCommitter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AutoCommitter
+//
+//  Created by Richie on 9/25/18.
+//  Copyright © 2018 Richie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ AutoCommitter/AutoCommitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoCommitter
+//
+//  Created by Richie on 9/25/18.
+//  Copyright © 2018 Richie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
